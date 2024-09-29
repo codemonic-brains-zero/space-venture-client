bbf77c0f4e9369dd55e66753c6fcc8912054eeba
codemonic-brains-zero <piyushmfalke@gmail.com>
2024-09-29 13:21:01 +0530

    app file changes

--- src/App.jsx
+++ src/App.jsx
@@ -18,44 +18,40 @@ import MessOutlets from "./pages/mess/MessOutlets";
 
 function App() {
   return (
-    <UserDashoard/>
-    );
-  }
-  
-  export default App;
+  <AuthProvider>
+      <Router>
+        <Routes>
+          {/* Public Routes */}
+            <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+            <Route path="/" element={<Home />} />
 
+          {/* Protected Routes */}
+            <Route element={<Layout />}>
+            <Route element={<ProtectedRoute allowedTypes={["User"]} />}>
+              <Route path="/user-dashboard" element={<UserDashboard />} />
+              <Route path="/book-rooms" element={<BookRooms />} />
+              <Route path="/rent-a-room" element={<RentARoom />} />
+              <Route path="/room-mates" element={<RoomMates />} />
+                <Route path="/mess-outlets" element={<MessOutlets />} />
+            </Route>
 
-  // <AuthProvider>
-  //     <Router>
-  //       <Routes>
-  //         {/* Public Routes */}
-  //           <Route path="/login" element={<Login />} />
-  //         <Route path="/signup" element={<SignUp />} />
-  //           <Route path="/" element={<Home />} />
+            <Route element={<ProtectedRoute allowedTypes={["Residency Owner"]} />}>
+              <Route path="/residency-owner-dashboard" element={<ResidencyOwnerDashboard />} />
+              <Route path="/register-room" element={<RegisterRoom />} />
+              </Route>
 
-  //         {/* Protected Routes */}
-  //           <Route element={<Layout />}>
-  //           <Route element={<ProtectedRoute allowedTypes={["User"]} />}>
-  //             <Route path="/user-dashboard" element={<UserDashboard />} />
-  //             <Route path="/book-rooms" element={<BookRooms />} />
-  //             <Route path="/rent-a-room" element={<RentARoom />} />
-  //             <Route path="/room-mates" element={<RoomMates />} />
-  //               <Route path="/mess-outlets" element={<MessOutlets />} />
-  //           </Route>
+              <Route element={<ProtectedRoute allowedTypes={["Multi-Mess Manager"]} />}>
+              <Route path="/multi-mess-manager-dashboard" element={<MultiMessManagerDashboard />} />
+            </Route>
+            </Route>
 
-  //           <Route element={<ProtectedRoute allowedTypes={["Residency Owner"]} />}>
-  //             <Route path="/residency-owner-dashboard" element={<ResidencyOwnerDashboard />} />
-  //             <Route path="/register-room" element={<RegisterRoom />} />
-  //             </Route>
+          {/* Redirect all other paths to login if not matched */}
+            <Route path="*" element={<Navigate to="/login" replace />} />
+        </Routes>
+      </Router>
+      </AuthProvider> 
+);
+}
 
-  //             <Route element={<ProtectedRoute allowedTypes={["Multi-Mess Manager"]} />}>
-  //             <Route path="/multi-mess-manager-dashboard" element={<MultiMessManagerDashboard />} />
-  //           </Route>
-  //           </Route>
-
-  //         {/* Redirect all other paths to login if not matched */}
-  //           <Route path="*" element={<Navigate to="/login" replace />} />
-  //       </Routes>
-  //     </Router>
-  //     </AuthProvider> 
- 
+export default App;
