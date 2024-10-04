9b54bed3f5ad415eea58fcacde768363083aaad7
SanjanaMankar <sanjanamankar22@gnmail.com>
2024-10-04 20:29:17 +0530

    "pages added to mess card"

--- src/components/mess/MessCard.jsx
+++ src/components/mess/MessCard.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // Importing Link from react-router-dom
 import MessNav from './MessNav';
 
 const MessCard = ({ name, image, description, rating }) => {
   return (
-
     <>
       <div>
-        <MessNav></MessNav>
+        <MessNav />
       </div>
       <div className="flex bg-[#efe1de] rounded-lg shadow-md overflow-hidden">
         <div className="flex-shrink-0">
@@ -21,12 +21,19 @@ const MessCard = ({ name, image, description, rating }) => {
           </div>
           <div className="mt-6">
             <div className="flex space-x-4">
-              <button className="bg-[#5b3846] text-white px-4 py-2 rounded-md hover:bg-white hover:text-black">
-                Contact Now
-              </button>
-              <button className="bg-pink-500 text-black px-4 py-2 rounded-md hover:bg-black hover:text-white">
-                Explore More
-              </button>
+          
+              <Link to='/ContactUs'>
+                <button className="bg-[#5b3846] text-white px-4 py-2 rounded-md hover:bg-white hover:text-black">
+                  Contact Now
+                </button>
+              </Link>
+              
+              
+              <Link to="/ExploreNow">
+                <button className="bg-pink-500 text-black px-4 py-2 rounded-md hover:bg-black hover:text-white">
+                  Explore More
+                </button>
+              </Link>
             </div>
             <div className="mt-4">
               <span className="inline-block bg-yellow-400 text-yellow-800 font-semibold px-2 py-1 rounded">
@@ -39,6 +46,5 @@ const MessCard = ({ name, image, description, rating }) => {
     </>
   );
 };
-
 
 export default MessCard;
